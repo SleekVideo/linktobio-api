dfde57c6a37c71ac4bf3987329cab9502991849b
Fortan Pireva <fortanpireva99@gmail.com>
2022-09-13 20:38:41 +0200

    added  nest passport for authentication in the sys

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
+import { Request } from '@nestjs/common';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() request) {
+    return this.authService.login(request.user);
+  }
+  @UseGuards(JwtGuard)
+  @Get('profile')
+  async getProfile(@Request() req) {
+    return req.user;
   }
 }
